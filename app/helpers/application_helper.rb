c5eec5199340aaa960d3044874b9c497be1c8b13
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-06-22 20:18:29 -0300

    Gems adjust

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def date_br(date_us)
         date_us.strftime("%d/%m/%Y")
     end
+    
+    def ambient_rails
+        if Rails.env.development?
+            "Develovimento"
+        elsif  Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
 end
